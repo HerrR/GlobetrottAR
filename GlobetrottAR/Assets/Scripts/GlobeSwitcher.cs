236c0e87774ef2f2442d0726bfd3de2e728d2ff9
HerrR <rbergel@kth.se>
2016-11-28 17:27:17 +0100

    Updated image database

--- GlobetrottAR/Assets/Scripts/GlobeSwitcher.cs
+++ GlobetrottAR/Assets/Scripts/GlobeSwitcher.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 using Vuforia;
 
 //[RequireComponent(typeof (VuforiaBehaviour))]
 public class GlobeSwitcher : MonoBehaviour {
+	Canvas developerUI;
 
 	// Use this for initialization
 	void Start () {
-		
+		developerUI = GameObject.Find ("DeveloperFeedback").GetComponent<Canvas> ();
 	}
 	
 	// Update is called once per frame
@@ -21,44 +23,48 @@ public class GlobeSwitcher : MonoBehaviour {
 		//(i.e. the ones currently being tracked by Vuforia)
 		IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours ();
 
-//		struct sortableTrackables;
 		TrackableBehaviour bestTrack = null;
 		float closestTrack = 0f;
 
-		// Iterate through the list of active trackables
-		Debug.Log ("List of trackables currently active (tracked): ");
+		string trackablesList = "";
+		string bestTrackString = "No current tracks";
+
 		foreach (TrackableBehaviour tb in activeTrackables) {
-			Debug.Log("Trackable: " + tb.TrackableName);
-
-
 			GameObject earthChild = tb.gameObject.transform.FindChild ("Earth").gameObject;
 
 			float diff = (earthChild.transform.position - transform.position).magnitude;
 
+			Vector3 normalVector = tb.transform.TransformDirection(0,-1,0);
+			Vector3 cameraFacingVector = gameObject.transform.TransformDirection(0,0,1);
+
+			float diffAngle = Vector3.Angle (normalVector, cameraFacingVector);
+
+			trackablesList += tb.TrackableName + " : "+diffAngle.ToString()+"\n";
 			earthChild.SetActive (false);
 
 			if(bestTrack == null){
 				Debug.Log("Best track set because of null check to "+tb.name);
 				bestTrack = tb;
-				closestTrack = diff;
+				closestTrack = diffAngle;
 				continue;
 			}
-
-			Debug.Log ("Multiple simultaneous trackers!");
-			Debug.Log (diff < closestTrack);
-
-			if (diff < closestTrack) {
+				
+			if (diffAngle < closestTrack) {
 				Debug.Log ("New best track found because of distance, set to: "+tb.name);
 				bestTrack = tb;
 				closestTrack = diff;
 			}
 		}
 
+		developerUI.gameObject.transform.FindChild ("Number of Tracks").GetComponent<Text> ().text = trackablesList;
+
 		if (bestTrack != null) {
 			Debug.Log ("Best track: "+bestTrack.gameObject.name);
+			bestTrackString = bestTrack.gameObject.name;
 			bestTrack.gameObject.transform.FindChild ("Earth").gameObject.SetActive (true);
+			developerUI.gameObject.transform.FindChild ("Current Track").GetComponent<Text> ().text = bestTrack.gameObject.name;
 		}
-		
+
 
 	}
 }
