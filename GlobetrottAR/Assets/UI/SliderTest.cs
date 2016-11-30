f0785099aba63712f71a1e6f6c8dc29de5e499d5
kmja <karl.mj.andersson@gmail.com>
2016-11-30 16:22:21 +0100

    GUI with popup that pops up at certain value

---
+++ GlobetrottAR/Assets/UI/SliderTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderTest : MonoBehaviour {
+
+	public Slider mainSlider;
+	public GameObject popup;
+
+	// Use this for initialization
+	void Start () {
+		// Add listener for value changes in slider
+		mainSlider.onValueChanged.AddListener (delegate {
+			ValueChangeCheck (mainSlider.GetComponent<Slider>().value);
+		});
+
+		// Initialize popup and hide as default
+		popup = GameObject.Find("Popup");
+		popup.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ValueChangeCheck(float newValue) {
+
+		if (newValue >= 0.5) {
+			popup.SetActive (true);
+		} else {
+			popup.SetActive (false);
+		}
+	
+	}
+}
